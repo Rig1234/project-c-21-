eaf597ae407b8602feb84675c68a51cf8480cf9f
Rig1234 <78023252+Rig1234@users.noreply.github.com>
2021-05-08 11:03:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function setup(){
 
     ball = createSprite(random(20,750),100, 40,40);
     ball.shapeColor = rgb(255,255,255);
-    ball.velocityY=5;
+    ball.velocityY= 5;
 
 }
 
@@ -39,14 +39,18 @@ function draw() {
     //write code to bounce off ball from the block1 
     if(block1.isTouching(ball) && ball.bounceOff(block1)){
         ball.shapeColor = "blue";
-        music.play();
+        ball.velocityX = -3;
+        ball.velocityY = -5;
+        music.stop();
     }
 
 
 
     if(block2.isTouching(ball) && ball.bounceOff(block2)){
         ball.shapeColor = "orange";
-        ball.velocityX = 2;
+        ball.velocityX = -2;
+        ball.velocityY = -3;
+        music.play();
         
         
         
@@ -57,14 +61,18 @@ function draw() {
     //write code to bounce off ball from the block3
     if (block3.isTouching(ball) && ball.bounceOff(block3)){
         ball.shapeColor = "red";
-        ball.velocityX = 2;
+        ball.velocityX = -3;
+        ball.velocityY = -7;
+        music.stop();
         
     }
 
     //write code to bounce off ball from the block4
     if (block4.isTouching(ball) && ball.bounceOff(block4)) {
         ball.shapeColor = "pink";
-        ball.velocityX = 2;
+        ball.velocityX = -2;
+        ball.velocityY = -2;
+        music.play();
         
     }
     drawSprites();
